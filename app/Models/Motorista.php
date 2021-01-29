2c295fbfcbfb56334e3c8aee55ae2abdf03caed3
Alessandro Ribeiro Frias <alessandro.frias@unicesumar.edu.br>
2021-01-29 11:16:00 -0300

    #TASK006 - Alteração primarykey and table Models

--- app/Models/Motorista.php
+++ app/Models/Motorista.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Motorista extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_motorista';
+    protected $table = 'motorista';
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/endereco', 'App\Http\Controllers\ControllerEndereco@getEndereco');
+
+Route::group(['prefix' => 'caroneiro'], function () {
+    Route::get('', 'App\Http\Controllers\ControllerCaroneiro@index');
+    Route::get('/{id_caroneiro}', 'App\Http\Controllers\ControllerCaroneiro@show');
+    Route::post('', 'App\Http\Controllers\ControllerCaroneiro@store');
+    Route::put('', 'App\Http\Controllers\ControllerCaroneiro@update');
+    Route::delete('/{id_caroneiro}', 'App\Http\Controllers\ControllerCaroneiro@update');
+});
